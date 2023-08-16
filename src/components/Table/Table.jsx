bb9740221b6b6abe71814bd743144c26e73de5a5
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-16 13:15:13 -0700

    feat: import table section into component.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,7 +1,20 @@
 import './Table.css';
 
 function Table() {
-	return <section id="table-section">Table</section>;
+	return (
+		<section id="table-section">
+			<table id="problem-table">
+				<tr>
+					<th>#</th>
+					<th>Question</th>
+					<th>Source</th>
+					<th>Categories</th>
+					<th>Date</th>
+					<th>Time Spent</th>
+				</tr>
+			</table>
+		</section>
+	);
 }
 
 export default Table;

c6b04bd612f72becc7fef2fa9cb8031c363c9c0c
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-16 13:22:50 -0700

    feat: import deletion section into component.

--- src/components/Deletion/Deletion.jsx
+++ src/components/Deletion/Deletion.jsx
@@ -1,7 +1,37 @@
 import './Deletion.css';
 
 function Deletion() {
-	return <section id="deletion-section">Deletion</section>;
+	return (
+		<section id="deletion-section">
+			<div className="deleteRow-div">
+				<label className="delete-label" htmlFor="deleteRow">
+					Problem number to delete:
+				</label>
+				<input
+					id="deleteRow"
+					name="deleteRow"
+					type="number"
+					placeholder="0"
+				></input>
+				<button id="deletButton" className="submit" onClick="deleteRow()">
+					Delete Row
+				</button>
+			</div>
+			<div className="deleteRow-div">
+				<label className="clear-label" htmlFor="clearButton">
+					Clear All Data:
+				</label>
+				<button
+					id="clearButton"
+					name="clearButton"
+					className="submit"
+					onClick="clearData()"
+				>
+					Clear
+				</button>
+			</div>
+		</section>
+	);
 }
 
 export default Deletion;

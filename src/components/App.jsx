71483ca51bbe0cf031addb3be61f450dff7deb52
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-17 13:25:52 -0700

    feat: Input divides into two sections.
    When screen approaches 600px width or less, input fields change layout from flex-column to flex-row.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import Form1 from './Form1/Form1';
 import Insertion from './Insertion/Insertion';
 import Table from './Table/Table';
 import Deletion from './Deletion/Deletion';
@@ -13,7 +12,6 @@ export default function App() {
 					Keep track of your progress in data-structure and algorithm problems
 				</h2>
 			</header>
-			<Form1 />
 			<Insertion />
 			<Table />
 			<Deletion />
--- src/components/Insertion/Insertion.jsx
+++ src/components/Insertion/Insertion.jsx
@@ -47,59 +47,63 @@ export default function Insertion() {
 	return (
 		<section id="insertion-section">
 			<div className="input-fields">
-				<Input
-					identity={'question'}
-					label={'Question'}
-					type={'text'}
-					placeholder={'e.g. Reverse Array'}
-					handleChange={handleQuestion.bind(this)}
-				/>
-				<Input
-					identity={'source'}
-					label={'Source'}
-					type={'text'}
-					placeholder={'e.g. LeetCode #23'}
-					handleChange={handleSource.bind(this)}
-				/>
-				<Input
-					identity={'categories'}
-					label={'Categories'}
-					type={'text'}
-					placeholder={'e.g. array, DFS, linked-list'}
-					handleChange={handleCategories.bind(this)}
-				/>
-				<div className="input-div">
-					<label htmlFor="difficulty">Difficulty</label>
-					<select
-						name="difficulty"
-						id="difficulty"
-						defaultValue="none-d"
-						onChange={handleDifficulty}
-					>
-						<option value="none-d" disabled>
-							Select difficulty
-						</option>
-						<option value="easy">Easy</option>
-						<option value="medium">Medium</option>
-						<option value="hard">Hard</option>
-					</select>
+				<div className="input-left">
+					<Input
+						identity={'question'}
+						label={'Question'}
+						type={'text'}
+						placeholder={'e.g. Reverse Array'}
+						handleChange={handleQuestion.bind(this)}
+					/>
+					<Input
+						identity={'source'}
+						label={'Source'}
+						type={'text'}
+						placeholder={'e.g. LeetCode #23'}
+						handleChange={handleSource.bind(this)}
+					/>
+					<Input
+						identity={'categories'}
+						label={'Categories'}
+						type={'text'}
+						placeholder={'e.g. array, DFS, linked-list'}
+						handleChange={handleCategories.bind(this)}
+					/>
 				</div>
-				<Input
-					identity={'date'}
-					label={'Date'}
-					type={'date'}
-					placeholder={'e.g. 1/2/2023'}
-					handleChange={handleDate.bind(this)}
-				/>
-				<Input
-					identity={'time'}
-					label={'Time'}
-					type={'number'}
-					placeholder={'e.g. 60'}
-					handleChange={handleTime.bind(this)}
-				/>
+				<div className="input-right">
+					<div className="input-div">
+						<label htmlFor="difficulty">Difficulty</label>
+						<select
+							name="difficulty"
+							id="difficulty"
+							defaultValue="none-d"
+							onChange={handleDifficulty}
+						>
+							<option value="none-d" disabled>
+								Select difficulty
+							</option>
+							<option value="easy">Easy</option>
+							<option value="medium">Medium</option>
+							<option value="hard">Hard</option>
+						</select>
+					</div>
+					<Input
+						identity={'date'}
+						label={'Date'}
+						type={'date'}
+						placeholder={'e.g. 1/2/2023'}
+						handleChange={handleDate.bind(this)}
+					/>
+					<Input
+						identity={'time'}
+						label={'Time'}
+						type={'number'}
+						placeholder={'e.g. 60'}
+						handleChange={handleTime.bind(this)}
+					/>
+				</div>
 			</div>
-			<div className="categories-selected">
+			{/* <div className="categories-selected">
 				<p id="categories-selected_title">Selected Categories (max 5):</p>
 				<div id="item_window">
 					<p>Strongly Connected Component</p>
@@ -108,7 +112,7 @@ export default function Insertion() {
 					<p>Strongly Connected Component</p>
 					<p>Strongly Connected Component</p>
 				</div>
-			</div>
+			</div> */}
 			<button id="enter-button" className="submit" onClick={handleInput}>
 				Enter
 			</button>

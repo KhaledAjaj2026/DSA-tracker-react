1304bdc9879f1cee7b89086a67866a6b36555529
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-16 18:24:33 -0700

    feat: solve creating dynamic table data from predetermined objects.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,24 +1,51 @@
 import './Table.css';
 
-// load localStorage into table with dynamically-created elements.
+const dynamicItems = [
+	{
+		name: 'Dynof',
+		description: 'weilds three axes.',
+	},
+	{
+		name: 'Rivera',
+		description: 'holds the ever-crystal.',
+	},
+	{
+		name: 'Credulle',
+		description: 'memorized the Greater Archives.',
+	},
+];
 
 function Table() {
 	return (
-		<section id="table-section">
-			<table id="problem-table">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Question</th>
-						<th>Source</th>
-						<th>Categories</th>
-						<th>Difficulty</th>
-						<th>Date</th>
-						<th>Time Spent</th>
-					</tr>
-				</thead>
-			</table>
-		</section>
+		<>
+			<section id="table-section">
+				<table id="problem-table">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Question</th>
+							<th>Source</th>
+							<th>Categories</th>
+							<th>Difficulty</th>
+							<th>Date</th>
+							<th>Time Spent</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							{dynamicItems.map((item) => (
+								<td key={item.name}>{item.name}</td>
+							))}
+						</tr>
+					</tbody>
+				</table>
+			</section>
+			{/* <div>
+				{dynamicItems.map((item) => (
+					<div key={item.name}>{item.name}</div>
+				))}
+			</div> */}
+		</>
 	);
 }
 

69807e238adf303534a434c2e264bf8de08712a5
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-20 16:18:03 -0700

    removed img tag around svg arrow-up.

--- src/components/ScrollWidget/ScrollWidget.jsx
+++ src/components/ScrollWidget/ScrollWidget.jsx
@@ -39,12 +39,26 @@ export default function ScrollWidget() {
 				scrollUp();
 			}}
 		>
-			<img
-				src="src/assets/arrow-up.svg"
-				width={80}
-				height={80}
-				alt="scroll to top"
-			/>
+			<svg
+				width="80px"
+				height="80px"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					opacity="0.15"
+					d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+					fill="none"
+				/>
+				<path
+					d="M12 7V17M12 7L16 11M12 7L8 11M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+					stroke="#dedede"
+					strokeWidth="1"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+			</svg>
 		</button>
 	);
 }

7c78e0f624f5f9e7cd665ba2eaf9621f80bedd70
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-10-04 12:48:25 -0700

    feat: make first-child of each content diff. color.
    When in mobile view, table turns into blocks of information, and each first-child of each block (i.e. problem number) will have larger text and the same BG-color as the 'th' tag.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,36 +1,5 @@
 import './Table.css';
 
-/* test data for populating localStorage. */
-// const tableLocalData = [
-// 	{
-// 		question: 'merge linked lists',
-// 		source: 'LC #41',
-// 		categories: 'linked-lists, two pointers',
-// 		difficulty: 'hard',
-// 		date: '2023-03-21',
-// 		time: '73',
-// 	},
-// 	{
-// 		question: 'flatten array',
-// 		source: 'CTCI #864',
-// 		categories: 'array, string, nesting',
-// 		difficulty: 'medium',
-// 		date: '2022-11-12',
-// 		time: '58',
-// 	},
-// 	{
-// 		question: 'In-Order Binary Tree Traversal',
-// 		source: 'LC #1266',
-// 		categories: 'tree, DFS, in-order, queue, recursion',
-// 		difficulty: 'easy',
-// 		date: '2023-01-03',
-// 		time: '113',
-// 	},
-// ];
-
-// localStorage.setItem('table-data', JSON.stringify(tableLocalData));
-/* end test data */
-
 /** Variable holding parsed data in localStorage. Data is an array holding objects. */
 const tableData = localStorage.getItem('table-data')
 	? JSON.parse(localStorage.getItem('table-data'))
